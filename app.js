bd858e4c9f812f65e296c8f7bd010b2bcf44e2aa
VasiliyVolkov <vasiliy.volkov88@gmail.com>
2020-02-03 20:14:13 -0800

    Java Complete

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const pics = [
+var pics = [
+"imgs/kitty_bed.JPG",
+"imgs/kitty_basket.JPG",
+"imgs/kitty_laptop.jpg",
+"imgs/kitty_door.jpg",
+"imgs/kitty_sink.JPG",
+"imgs/kitty_wall.jpg"
+];
 
-]
+var btn  = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 1;
 
-const button  = document.querySelector('button')
-const img = document.querySelector('img')
-let index = 0
-button.addEventListener('click', function(){
-index += 1
-if  (index === pics.length)
-  img.src = pics[index]
-
-})
+btn.addEventListener("click", function(){
+if  (counter === 6){
+  counter = 0
+}
+  img.src = pics[counter]
+  counter = counter + 1;
+});
